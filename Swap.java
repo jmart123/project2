7923b73026e8d46ea550fd04b99eba85b4993fe3
Jeffrey Philip Martin <jmart123@hydra13.eecs.utk.edu>
2018-04-24 16:04:07 -0400

    using getStackPointer for Swap conditions

--- Swap.java
+++ Swap.java
@@ -3,10 +3,10 @@ public class Swap implements Instruction
 		public void run(vmStack vms){
 				//case with empty stack
 
-			 	if(vms.size() == 0) return;
+			 	if(vms.getStackPointer == -1) return;
 
 				//need case with only 1 value on the stack
-			 	if(vms.size() == 1) vms.push(0);
+			 	if(vms.getStackPointer == 0) vms.push(0);
 
 				//case where there are two or more values on the stack
 				int tmp;
