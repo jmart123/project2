64b5c6faac01a5fd57506bbb23574fc08a9d25be
Jeffrey Philip Martin <jmart123@hydra15.eecs.utk.edu>
2018-04-26 13:29:57 -0400

    JM: merging processor

--- Processor.java
+++ Processor.java
@@ -1,11 +1,22 @@
 // Driver file
 
 class Processor {
+  private List<Integer> instList;
+  private vmStack vms;
+
 	public static void main(String[] args) {
 		Parser p = new Parser(args[0]);
 		
 		for () {
 			System.out.println(i);
 		}
+
+    instList = p.getInstructions();
+    vms = new vmStack(); 
+
+
+		for(int i = 0; i < instList.size(); i++){
+		  instList.get(i).run(vms, i);    
+    }
 	}
 }
